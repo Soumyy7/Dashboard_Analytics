df21a4cf7751841d42c308b0ecfc8f8789ccbe88
Soumy Prajapati <symbols.soumy7@gmail.com>
2024-03-17 17:41:02 +0530

    successfully added the chatbot into the dashboard

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,4 @@
+import ChatBot from "views/ChatBot";
 import Dashboard from "views/Dashboard.js";
 import TableList from "views/TableList.js";
 
@@ -5,7 +6,6 @@ var routes = [
   {
     path: "/dashboard",
     name: "Dashboard",
-    rtlName: "لوحة القيادة",
     icon: "tim-icons icon-chart-pie-36",
     component: <Dashboard />,
     layout: "/admin",
@@ -13,9 +13,16 @@ var routes = [
   {
     path: "/tables",
     name: "Table List",
-    rtlName: "قائمة الجدول",
     icon: "tim-icons icon-puzzle-10",
     component: <TableList />,
+    layout: "/admin",
+  },
+  {
+    path: "/chatbot",
+    name: "ChatBot",
+    icon: "tim-icons icon-chat-33",
+    // icon: "tim-icons icon-support-17",
+    component: <ChatBot />,
     layout: "/admin",
   },
 ];
